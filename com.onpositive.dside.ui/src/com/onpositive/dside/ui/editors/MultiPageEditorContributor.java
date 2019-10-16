f3e4b93a5aac23cf43e5fc8e95ac373fc56f0d61
Dmitry Karpenko <dmitry@onpositive.com>
2019-10-16 17:01:48 +0700

    Remove mock SampleAction from UI
    Change-Id: Ie0d5a9afbe1c32d94f9079baaaf53d83995f4c9a

--- com.onpositive.dside.ui/src/com/onpositive/dside/ui/editors/MultiPageEditorContributor.java
+++ com.onpositive.dside.ui/src/com/onpositive/dside/ui/editors/MultiPageEditorContributor.java
@@ -1,13 +1,9 @@
 package com.onpositive.dside.ui.editors;
 
-import org.eclipse.jface.action.*;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.action.IAction;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IEditorPart;
-import org.eclipse.ui.IWorkbenchActionConstants;
-import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
-import org.eclipse.ui.ide.IDE;
 import org.eclipse.ui.ide.IDEActionFactory;
 import org.eclipse.ui.part.MultiPageEditorActionBarContributor;
 import org.eclipse.ui.texteditor.ITextEditor;
@@ -20,13 +16,11 @@ import org.eclipse.ui.texteditor.ITextEditorActionConstants;
  */
 public class MultiPageEditorContributor extends MultiPageEditorActionBarContributor {
 	private IEditorPart activeEditorPart;
-	private Action sampleAction;
 	/**
 	 * Creates a multi-page contributor.
 	 */
 	public MultiPageEditorContributor() {
 		super();
-		createActions();
 	}
 	/**
 	 * Returns the action registed with the given text editor.
@@ -79,25 +73,5 @@ public class MultiPageEditorContributor extends MultiPageEditorActionBarContribu
 				getAction(editor, IDEActionFactory.BOOKMARK.getId()));
 			actionBars.updateActionBars();
 		}
-	}
-	private void createActions() {
-		sampleAction = new Action() {
-			public void run() {
-				MessageDialog.openInformation(null, "Ui", "Sample Action Executed");
-			}
-		};
-		sampleAction.setText("Sample Action");
-		sampleAction.setToolTipText("Sample Action tool tip");
-		sampleAction.setImageDescriptor(PlatformUI.getWorkbench().getSharedImages().
-				getImageDescriptor(IDE.SharedImages.IMG_OBJS_TASK_TSK));
-	}
-	public void contributeToMenu(IMenuManager manager) {
-		IMenuManager menu = new MenuManager("Editor &Menu");
-		manager.prependToGroup(IWorkbenchActionConstants.MB_ADDITIONS, menu);
-		menu.add(sampleAction);
-	}
-	public void contributeToToolBar(IToolBarManager manager) {
-		manager.add(new Separator());
-		manager.add(sampleAction);
 	}
 }
