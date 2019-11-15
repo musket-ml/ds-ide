4d5eb827a25cd633609804307f90dbd6282a8c59
Dmitry Karpenko <dmitry@onpositive.com>
2019-11-15 20:25:40 +0700

    Perspective and very basic intro impl
    Change-Id: I077ac74e7e8aa6147be8c7c80bf2d40c427d6a7e

---
+++ com.onpositive.dside.ui/src/com/onpositive/dside/ui/perspective/DSIDEPerspectiveFactory.java
@@ -0,0 +1,9 @@
+package com.onpositive.dside.ui.perspective;
+
+import org.python.pydev.ui.perspective.PythonPerspectiveFactory;
+
+public class DSIDEPerspectiveFactory extends PythonPerspectiveFactory {
+	
+	public static final String PERSPECTIVE_ID = "com.onpositive.dside.ui.perspective";
+
+}
