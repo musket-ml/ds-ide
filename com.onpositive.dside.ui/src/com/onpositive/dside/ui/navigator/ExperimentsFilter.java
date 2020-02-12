26e06cc79d89f69bc19aee222a87ac150b92c4d2
Dmitry Karpenko <dmitry@onpositive.com>
2020-02-12 18:55:25 +0700

    Avoid excluding 'experiments' folder, when it's not a first level child of a project
    Change-Id: I22ec157e00c0f255e4a709d0f21a9652b9127abe

--- com.onpositive.dside.ui/src/com/onpositive/dside/ui/navigator/ExperimentsFilter.java
+++ com.onpositive.dside.ui/src/com/onpositive/dside/ui/navigator/ExperimentsFilter.java
@@ -1,6 +1,7 @@
 package com.onpositive.dside.ui.navigator;
 
 import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.jface.viewers.ViewerFilter;
 
@@ -9,14 +10,14 @@ import com.onpositive.dside.ui.IMusketConstants;
 public class ExperimentsFilter extends ViewerFilter {
 
 	public ExperimentsFilter() {
-		// TODO Auto-generated constructor stub
+		// Default constructor
 	}
 
 	@Override
 	public boolean select(Viewer viewer, Object parentElement, Object element) {
 		if (element instanceof IFolder) {
 			IFolder el=(IFolder)element;
-			if (el.getName().equals(IMusketConstants.MUSKET_EXPERIMENTS_FOLDER)) {
+			if (el.getName().equals(IMusketConstants.MUSKET_EXPERIMENTS_FOLDER) && el.getParent() instanceof IProject) {
 				return false;
 			}
 		}
